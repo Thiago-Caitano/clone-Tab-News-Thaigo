373e775f3f56ebddb8d24a676e58802f01ad311c
Thiago Caitano <55213675+Thiago-Caitano@users.noreply.github.com>
2026-08-12 19:28:59 +0000

    Adicionando um comentario de quando retornar

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 function Home() {
     return <h1> Okay, agora é para ir com essa mensagem para me lembrar do dia 12/08/26 o retorno ao curso</h1>
+            <h2> Tenho que voltar em 31/12/26 para ver o meu avanço</h2>
 }
 
 export default Home;

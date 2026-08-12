984c2e50dd7430f7e9e60672949a7227a3cca38f
Thiago Caitano <55213675+Thiago-Caitano@users.noreply.github.com>
2026-08-12 19:34:38 +0000

    agr é para ir

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 function Home() {
-    return <h1> Okay, agora é para ir com essa mensagem para me lembrar do dia 12/08/26 o retorno ao curso</h1>
-            <h2> Tenho que voltar em 31/12/26 para ver o meu avanço</h2>
+    return <><h1> Okay, agora é para ir com essa mensagem para me lembrar do dia 12/08/26 o retorno ao curso</h1><h2> Tenho que voltar em 31/12/26 para ver o meu avanço.</h2><h3>ta bom isso ficou um pouco mais complicado do q eu esperava</h3></>
 }
 
 export default Home;

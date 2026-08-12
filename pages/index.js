c1721eb0b680a70794ef13a0042e383dbe2c36e6
Thiago Caitano <55213675+Thiago-Caitano@users.noreply.github.com>
2026-08-12 18:48:10 +0000

    corringindo para lembrar do 'recomoço'

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1> Agora vai<h2>Subtítulo</h2> </h1>
+    return <h1> Okay, agora é para ir com essa mensagem para me lembrar do dia 12/08/26 o retorno ao curso</h1>
 }
 
 export default Home;
